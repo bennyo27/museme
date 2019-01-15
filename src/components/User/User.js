0033eb5fe047aeaf7ac91ffbb415dca5c2380a56
benny <oseguerabenny27@gmail.com>
2019-01-14 18:18:29 -0800

    Added: Follows

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchDbUser } from "../../store/actions/usersActions";
 import "./User.css";
+import axios from "axios";
 
 class User extends Component {
   componentDidMount() {
@@ -17,6 +18,18 @@ class User extends Component {
           <div className="user-name">
             <h1>{this.props.dbUser.display_name}</h1>
           </div>
+          <div
+            className="btn-follow"
+            onClick={() => {
+              axios.post(
+                `http://localhost:8888/users/${
+                  this.props.user.display_name
+                }/follows/${this.props.dbUser.display_name}`
+              );
+            }}
+          >
+            Follow
+          </div>
         </div>
       </div>
     );
@@ -26,6 +39,7 @@ class User extends Component {
 // mapStateToProps
 const mapStateToProps = state => {
   return {
+    user: state.spotifyReducer.user,
     dbUser: state.usersReducer.dbUser
   };
 };
